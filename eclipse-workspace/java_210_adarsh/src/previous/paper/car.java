02249d26772489cecf78e196beb62ceb3967fdd1
unknown <adarshkps1412@gmail.com>
2025-02-18 10:51:16 +0530

    eclipes file 3

---
+++ eclipse-workspace/java_210_adarsh/src/previous/paper/car.java
@@ -0,0 +1,24 @@
+package previous.paper;
+
+public class car extends vehicle{
+ 
+		
+		public void sunRoof()
+		{
+			System.out.println("this is the sunroof of a car");
+		}
+		public static void main(String[] args) {
+			car c1=new car();
+			
+			c1.accelator();
+			c1.sunRoof();
+		}
+		
+		
+		
+		
+		
+
+}
+
+
---
+++ eclipse-workspace/java_210_adarsh/src/previous/paper/vehicle.java
@@ -0,0 +1,15 @@
+package previous.paper;
+
+public class vehicle {
+	
+	public void start()
+	{
+		System.out.println("start feature of a vehicle");
+	}
+	public void accelator()
+	{
+		System.out.println("accelator of a vehicle");
+	}
+	
+	
+}
