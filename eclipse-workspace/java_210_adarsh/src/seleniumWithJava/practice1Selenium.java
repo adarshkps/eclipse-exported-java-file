211ac60a80b051afaf7f5d709cf4014d580b08cb
unknown <adarshkps1412@gmail.com>
2025-02-21 00:12:35 +0530

    file eclipes new 5

---
+++ eclipse-workspace/java_210_adarsh/src/seleniumWithJava/practice1Selenium.java
@@ -0,0 +1,14 @@
+package seleniumWithJava;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class practice1Selenium {
+
+	public static void main(String[] args) {
+
+		WebDriver driver =new ChromeDriver();
+		
+	}
+
+}
